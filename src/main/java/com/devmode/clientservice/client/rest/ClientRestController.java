9d4f8286658d81f4e4f534d58c2d662c738dedd8
dmitry.kovalenko <Dmitry.Kovalenko@artezio.com>
2023-06-15 14:54:12 +0300

    Добавил сваггер.

--- src/main/java/com/devmode/clientservice/client/rest/ClientRestController.java
+++ src/main/java/com/devmode/clientservice/client/rest/ClientRestController.java
@@ -4,6 +4,7 @@ import com.devmode.clientservice.auth.api.AuthorizedUser;
 import com.devmode.clientservice.client.ClientService;
 import com.devmode.clientservice.client.dto.ClientCreateRequest;
 import com.devmode.clientservice.client.dto.ClientPreview;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,17 +20,17 @@ public class ClientRestController {
     private final ClientService clientService;
 
     @GetMapping
-    public ResponseEntity<List<ClientPreview>> getByCurrentUser(@AuthenticationPrincipal AuthorizedUser user) {
+    public ResponseEntity<List<ClientPreview>> getByCurrentUser(@Parameter(hidden = true) @AuthenticationPrincipal AuthorizedUser user) {
         return ResponseEntity.ok(clientService.findAllByUserId(user.getLocalId()));
     }
 
     @PostMapping
-    public ResponseEntity<Integer> create(ClientCreateRequest createRequest, @AuthenticationPrincipal AuthorizedUser user) {
+    public ResponseEntity<Integer> create(ClientCreateRequest createRequest, @Parameter(hidden = true) @AuthenticationPrincipal AuthorizedUser user) {
         return ResponseEntity.ok(clientService.create(createRequest, user.getLocalId()));
     }
 
     @DeleteMapping("/{id:\\d+}")
-    public ResponseEntity<Void> delete(@PathVariable("id") Integer id, @AuthenticationPrincipal AuthorizedUser user) {
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id, @Parameter(hidden = true) @AuthenticationPrincipal AuthorizedUser user) {
         clientService.delete(id, user.getLocalId());
         return ResponseEntity.ok().build();
     }
